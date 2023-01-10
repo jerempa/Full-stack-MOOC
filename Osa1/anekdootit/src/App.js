edfeab324179c2aebfea4467c10eab032bdb38f8
jerempa <115798732+jerempa@users.noreply.github.com>
2023-01-10 19:24:49 +0200

    Final submission for exercises 1.12-1.14

---
+++ Osa1/anekdootit/src/App.js
@@ -0,0 +1,67 @@
+import { useState } from 'react'
+
+
+const Anecdote = (props) => {
+  return(
+    <div>
+      <h1> Anecdote of the day </h1> 
+      <p> {props.arr[props.value]}</p>
+      <p> has {props.votecount} votes </p>
+    </div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <div>
+      <button onClick={props.click}> {props.text} </button> 
+    </div>
+  )
+}
+
+const MostVotes = (props) => {
+  const maxIndex = props.votes.indexOf(Math.max(...props.votes)); //index of the anecdote with most votes
+  return(
+    <div>
+      <h1> Anecdote with most votes </h1>
+      <p> {props.anecdotes[maxIndex]} </p>
+    </div>
+  )
+}
+
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.'
+  ]
+  const [votes, setVotes] = useState(new Array(7).fill(0), 0); //array for counting votes
+
+  const [selected, setSelected] = useState(0)
+
+  const randomValue = () => {
+    setSelected(Math.floor(Math.random() * 7)) //select a random anecdote the array
+  }
+
+  const incrementValue = () => {
+    let copy = [...votes];
+    copy[selected] += 1;
+    setVotes(copy); //increment value when the button is pressed
+  }
+
+  return (
+    <div>
+      <Anecdote arr={anecdotes} value={selected} votecount={votes[selected]} />
+      <Button click={incrementValue} text="vote"/> 
+      <Button click={randomValue} text="next anecdote"/>
+      <MostVotes anecdotes={anecdotes} votes={votes}/>
+    </div>
+  )
+}
+
+export default App
